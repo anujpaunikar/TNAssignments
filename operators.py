af7cb9652cd344af2f4c27dc0e0bebfb76368922
anujpaunikar <111754073+anujpaunikar@users.noreply.github.com>
2022-08-30 17:05:09 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,26 @@
+a=6
+b=5
+
+#Arithmetic Operators
+print("Addition :", a+b)
+print("Substaction :", a-b)
+print("Multiplication :", a*b)
+print("Division :", a/b)
+print("Modulus :", a%b)
+
+#Comparision Operators
+print("A equal to B :", a==b)
+print("A not equal to B :", a!=b)
+print("A is greater than B :", a>b)
+print("A is less than B :", a<b)
+print("A is greater than or equal to B :", a>=b)
+print("A is less than or equal to B :", a<=b)
+
+#Logical Opearators
+print("A is greater than 5 and less than 10 :", a>5 and a<10) #If both conditions are true, then true
+print("B is greater than 5 or less than 10 :", b>5 or b<10) #If one of the condition is true, then true
+print("Reverse the numbers and check if A is greater than B :", not(a>b))
+
+print("'=' operator will assign the variable(eg: b=6) :")
+b=8
+print("b :",b)
